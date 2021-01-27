05835af5117b55ea5980e83f9b7deaf2ab6e9f8b
aragonec <47608292+aragonec@users.noreply.github.com>
2021-01-26 22:03:59 -0700

    introduccion prolog

--- funcional/introduccion/funciones.hs
+++ funcional/introduccion/funciones.hs
@@ -2,8 +2,6 @@
 -- public void suma (a,b){returb a+b}
 -- ghc -o ejecutable ./funciones.hs crear ejecutable
 -- ghci entrar al programa prelude
-main-::-IO-()
-main-=return-() 
 suma a b = a + b
 resta a b = a - b
 sumar a b c d e = a + b + c + d + e 
